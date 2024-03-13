c5878c32367ad139cf2ba02140c1abc1662d5073
Robert Steven Delgado Peralta <158115699+DrowAnn@users.noreply.github.com>
2024-03-13 18:04:12 +0000

    Finalización del proyecto con el método de comparación manual y utilizando la libreria java.text.collator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 /*En la fila de un banco hay 25 personas ubicadas de manera aleatoria. Ordenar la fila teniendo en cuenta el nombre de las personas en orden alfabético.
 
 - Se desarrolla una clase que toma 25 nombres por defecto o la cantidad que desee el usuario y los agregua a un arreglo en el orden en que se capturan.
-- Se desarrolla una clase que toma el arreglo previamente llenado y utiliza el algoritmo de ordenamiento por inserción para ordenar los nombres en orden alfabético.
+- Se desarrolla una clase que toma el arreglo previamente llenado y utiliza el algoritmo de ordenamiento por inserción para ordenar los nombres en orden alfabético. Este se realizo de dos formas, con uso de la libreria java.text.Collator y por comparacion letra a letra, programado manualmente.
 - Se desarrolla una clase que imprime los nombres por pantalla.*/
 
 import java.util.Scanner;
@@ -10,21 +10,49 @@ public class Main {
 
     public static void main(String[] args) {
         
-        String[] arreglo;
+        String[] arreglo = {};
+        String[] arregloM = {};
+        Integer repetidor;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite la cantidad de personas en la fila:");
-        Integer longitud = scanner.nextInt();
-        System.out.println();
+        System.out.println("Desea digitar una cantidad de persona o usar la predeterminada (25)?. Digite el numero de la opción que corresponda a continuación:");
+        
+        do{
+            System.out.println();
+            System.out.println("1. Digitar cantidad");
+            System.out.println("2. Cantidad predeterminada (25)");
+            System.out.println();
+            Integer opcion = scanner.nextInt();
+            System.out.println();
+            repetidor = 0;
+            if (opcion == 1){
+                System.out.println("Digite la cantidad de personas en la fila:");
+                Integer longitud = scanner.nextInt();
+                System.out.println();
+                EscritorArreglo escritor = new EscritorArreglo(longitud);
+                arreglo = escritor.escribirArreglo();
+                arregloM = arreglo;
+            }else if(opcion == 2){
+                EscritorArreglo escritor = new EscritorArreglo();
+                arreglo = escritor.escribirArreglo();
+                arregloM = arreglo;
+            }else{
+                System.out.println("Opcion incorrecta");
+                repetidor = 1;
+            }
+        }while (repetidor != 0);
 
-        EscritorArreglo escritor = new EscritorArreglo(longitud);
-        arreglo = escritor.escribirArreglo();
+        /*OrdenamientoInsercion arregloOrdenadoC = new OrdenamientoInsercion(arreglo);
+        arreglo = arregloOrdenadoC.OrdenadorCollator();*/
 
-        OrdenamientoInsercion arregloOrdenado = new OrdenamientoInsercion(arreglo);
-        arreglo = arregloOrdenado.Ordenador();
+        OrdenamientoInsercion arregloOrdenadoM = new OrdenamientoInsercion(arregloM);
+        arregloM = arregloOrdenadoM.OrdenadorManual();
 
-        ImprimirArreglos impresion = new ImprimirArreglos(arreglo);
-        impresion.impresionArreglo();
+        /*ImprimirArreglos impresion = new ImprimirArreglos(arreglo);
+        impresion.impresionArreglo();*/
+
+        ImprimirArreglos impresionM = new ImprimirArreglos(arregloM);
+        impresionM.impresionArreglo();
 
         scanner.close();
     }
--- src/main/java/OrdenamientoInsercion.java
+++ src/main/java/OrdenamientoInsercion.java
@@ -13,7 +13,7 @@ public class OrdenamientoInsercion {
     }
 
     // Declaración de los Metodos
-    public String[] Ordenador() {
+    public String[] OrdenadorCollator() {
         Collator collator = Collator.getInstance();
         String temporal;
 
@@ -30,6 +30,52 @@ public class OrdenamientoInsercion {
             }
         }
         
+        return arreglo;
+    }
+    //Ordenamiento letra a letra manual
+    public String[] OrdenadorManual() {
+        String temporal;
+        int igualdad;
+
+        for (int i = 1; i < arreglo.length; i++){
+            for (int j = i; j > 0; j--){
+                igualdad = 0;
+                if (arreglo[j].length() <= arreglo[j - 1].length()){
+                    for (int k = 0; k < arreglo[j].length(); k++){
+                        if (arreglo[j].charAt(k) < arreglo[j - 1].charAt(k)){
+                            temporal = arreglo[j - 1];
+                            arreglo[j - 1] = arreglo[j];
+                            arreglo[j] = temporal;
+                            break;
+                        }else if (arreglo[j].charAt(k) > arreglo[j - 1].charAt(k)){
+                            break;
+                        }else{
+                            igualdad++;
+                        }
+                    }
+                    if (igualdad == arreglo[j].length()){
+                        temporal = arreglo[j - 1];
+                        arreglo[j - 1] = arreglo[j];
+                        arreglo[j] = temporal;
+                    }
+                }else{
+                    for (int k = 0; k < arreglo[j - 1].length(); k++){
+                        if (arreglo[j].charAt(k) < arreglo[j - 1].charAt(k)){
+                            temporal = arreglo[j - 1];
+                            arreglo[j - 1] = arreglo[j];
+                            arreglo[j] = temporal;
+                            break;
+                        }else if (arreglo[j].charAt(k) > arreglo[j - 1].charAt(k)){
+                            break;
+                        }
+                    }
+                    if (igualdad == arreglo[j - 1].length()){
+                        break;
+                    }
+                }
+            }
+        }
+
         return arreglo;
     }
 
