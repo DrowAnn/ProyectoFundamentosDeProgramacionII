209da47d8fc3ff61a43d2005d83feac93e1d5ec2
Robert Steven Delgado Peralta <158115699+DrowAnn@users.noreply.github.com>
2024-03-13 18:06:44 +0000

    Arreglo del formateo del código

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         String[] arreglo = {};
         String[] arregloM = {};
         Integer repetidor;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Desea digitar una cantidad de persona o usar la predeterminada (25)?. Digite el numero de la opción que corresponda a continuación:");
-        
-        do{
+        System.out.println(
+                "Desea digitar una cantidad de persona o usar la predeterminada (25)?. Digite el numero de la opción que corresponda a continuación:");
+
+        do {
             System.out.println();
             System.out.println("1. Digitar cantidad");
             System.out.println("2. Cantidad predeterminada (25)");
@@ -25,31 +26,31 @@ public class Main {
             Integer opcion = scanner.nextInt();
             System.out.println();
             repetidor = 0;
-            if (opcion == 1){
+            if (opcion == 1) {
                 System.out.println("Digite la cantidad de personas en la fila:");
                 Integer longitud = scanner.nextInt();
                 System.out.println();
                 EscritorArreglo escritor = new EscritorArreglo(longitud);
                 arreglo = escritor.escribirArreglo();
                 arregloM = arreglo;
-            }else if(opcion == 2){
+            } else if (opcion == 2) {
                 EscritorArreglo escritor = new EscritorArreglo();
                 arreglo = escritor.escribirArreglo();
                 arregloM = arreglo;
-            }else{
+            } else {
                 System.out.println("Opcion incorrecta");
                 repetidor = 1;
             }
-        }while (repetidor != 0);
+        } while (repetidor != 0);
 
-        /*OrdenamientoInsercion arregloOrdenadoC = new OrdenamientoInsercion(arreglo);
-        arreglo = arregloOrdenadoC.OrdenadorCollator();*/
+        OrdenamientoInsercion arregloOrdenadoC = new OrdenamientoInsercion(arreglo);
+        arreglo = arregloOrdenadoC.OrdenadorCollator();
 
         OrdenamientoInsercion arregloOrdenadoM = new OrdenamientoInsercion(arregloM);
         arregloM = arregloOrdenadoM.OrdenadorManual();
 
-        /*ImprimirArreglos impresion = new ImprimirArreglos(arreglo);
-        impresion.impresionArreglo();*/
+        ImprimirArreglos impresion = new ImprimirArreglos(arreglo);
+        impresion.impresionArreglo();
 
         ImprimirArreglos impresionM = new ImprimirArreglos(arregloM);
         impresionM.impresionArreglo();
--- src/main/java/OrdenamientoInsercion.java
+++ src/main/java/OrdenamientoInsercion.java
@@ -17,59 +17,61 @@ public class OrdenamientoInsercion {
         Collator collator = Collator.getInstance();
         String temporal;
 
-        //Se fija el valor PRYMARY para que no distinga entre Mayuscula, minuscula y acentos
+        // Se fija el valor PRYMARY para que no distinga entre Mayuscula, minuscula y
+        // acentos
         collator.setStrength(Collator.PRIMARY);
-        
-        for (int i = 1; i < arreglo.length; i++){
-            for (int j = i; j > 0; j--){
-                if (collator.compare(arreglo[j], arreglo[j - 1]) < 0){
+
+        for (int i = 1; i < arreglo.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (collator.compare(arreglo[j], arreglo[j - 1]) < 0) {
                     temporal = arreglo[j - 1];
                     arreglo[j - 1] = arreglo[j];
                     arreglo[j] = temporal;
                 }
             }
         }
-        
+
         return arreglo;
     }
-    //Ordenamiento letra a letra manual
+
+    // Ordenamiento letra a letra manual
     public String[] OrdenadorManual() {
         String temporal;
         int igualdad;
 
-        for (int i = 1; i < arreglo.length; i++){
-            for (int j = i; j > 0; j--){
+        for (int i = 1; i < arreglo.length; i++) {
+            for (int j = i; j > 0; j--) {
                 igualdad = 0;
-                if (arreglo[j].length() <= arreglo[j - 1].length()){
-                    for (int k = 0; k < arreglo[j].length(); k++){
-                        if (arreglo[j].charAt(k) < arreglo[j - 1].charAt(k)){
+                if (arreglo[j].length() <= arreglo[j - 1].length()) {
+                    for (int k = 0; k < arreglo[j].length(); k++) {
+                        if (arreglo[j].charAt(k) < arreglo[j - 1].charAt(k)) {
                             temporal = arreglo[j - 1];
                             arreglo[j - 1] = arreglo[j];
                             arreglo[j] = temporal;
                             break;
-                        }else if (arreglo[j].charAt(k) > arreglo[j - 1].charAt(k)){
+                        } else if (arreglo[j].charAt(k) > arreglo[j - 1].charAt(k)) {
                             break;
-                        }else{
+                        } else {
                             igualdad++;
                         }
                     }
-                    if (igualdad == arreglo[j].length()){
+                    if (igualdad == arreglo[j].length()) {
                         temporal = arreglo[j - 1];
                         arreglo[j - 1] = arreglo[j];
                         arreglo[j] = temporal;
                     }
-                }else{
-                    for (int k = 0; k < arreglo[j - 1].length(); k++){
-                        if (arreglo[j].charAt(k) < arreglo[j - 1].charAt(k)){
+                } else {
+                    for (int k = 0; k < arreglo[j - 1].length(); k++) {
+                        if (arreglo[j].charAt(k) < arreglo[j - 1].charAt(k)) {
                             temporal = arreglo[j - 1];
                             arreglo[j - 1] = arreglo[j];
                             arreglo[j] = temporal;
                             break;
-                        }else if (arreglo[j].charAt(k) > arreglo[j - 1].charAt(k)){
+                        } else if (arreglo[j].charAt(k) > arreglo[j - 1].charAt(k)) {
                             break;
                         }
                     }
-                    if (igualdad == arreglo[j - 1].length()){
+                    if (igualdad == arreglo[j - 1].length()) {
                         break;
                     }
                 }
